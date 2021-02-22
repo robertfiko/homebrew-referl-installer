a3afd810ee31716575c8406b62b1c7f45daed55c
Robert Fiko <fiko.robert@gmail.com>
2021-02-22 23:49:18 +0100

    0.3 p1

--- referl.rb
+++ referl.rb
@@ -1,13 +1,14 @@
 class Referl < Formula
       desc "A 'hello-world' kind of dummy installer."
       homepage "https://github.com/robertfiko/homebrew-referl"
-      url "https://github.com/robertfiko/referl/releases/download/0.2/referl-0.2.tar.gz"
+      url "https://github.com/robertfiko/referl/archive/0.3.tar.gz"
       #license
       sha256 "185d082716d0073ef582359ee6115ba1b02225caadfcfb6fa59b8b185e46d3ae"
     
       bottle :unneeded
     
       def install
+        system "g++", "main.cpp -o referl"
         bin.install "referl"
       end
     
