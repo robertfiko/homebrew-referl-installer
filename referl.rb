fd638e69e3b4ff53e60afb39e278113aa7592d48
Robert Fiko <64466886+robertfiko@users.noreply.github.com>
2021-02-22 23:56:34 +0100

    Update referl.rb

--- referl.rb
+++ referl.rb
@@ -8,7 +8,7 @@ class Referl < Formula
       bottle :unneeded
     
       def install
-        system "g++", "main.cpp -o referl"
+        system "g++", "main.cpp", "-o", "referl"
         bin.install "referl"
       end
     
