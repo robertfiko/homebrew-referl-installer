a6b821863f8d5350b83c9f0f53f108343d9b9d29
Robert Fiko <fiko.robert@gmail.com>
2021-02-22 23:33:28 +0100

    0.2

--- referl.rb
+++ referl.rb
@@ -1,7 +1,7 @@
 class Referl < Formula
       desc "A 'hello-world' kind of dummy installer."
       homepage "https://github.com/robertfiko/homebrew-referl"
-      url "https://github.com/robertfiko/referl/releases/download/0.2/referl-0.2.tar.gz"
+      url "https://github.com/robertfiko/homebrew-referl-installer/releases/download/0.2/referl-0.2.tar.gz"
       #license
       sha256 "7d98bb7779bd8036dc3d08f77ebb6b313b78d629e5ccbe1e17839c93f09866e7"
     
