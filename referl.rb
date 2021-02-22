d774e29195a17b2a0ae7fa53c1687056aa973736
Robert Fiko <fiko.robert@gmail.com>
2021-02-22 23:22:21 +0100

    Update 2.0

--- referl.rb
+++ referl.rb
@@ -3,7 +3,7 @@ class Referl < Formula
       homepage "https://github.com/robertfiko/homebrew-referl"
       url "https://github.com/robertfiko/homebrew-referl/releases/download/0.1/referl-0.1.tar.gz"
       #license
-      sha256 "7d98bb7779bd8036dc3d08f77ebb6b313b78d629e5ccbe1e17839c93f09866e7"
+      sha256 "185d082716d0073ef582359ee6115ba1b02225caadfcfb6fa59b8b185e46d3ae"
     
       bottle :unneeded
     
