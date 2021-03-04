deee777f0170e05340fc704a2853c6033e7ad179
Robert Fiko <fiko.robert@gmail.com>
2021-03-04 21:51:08 +0100

    I/O

--- referl.rb
+++ referl.rb
@@ -1,15 +1,18 @@
 class Referl < Formula
+  require 'colorize'
+
       desc "A 'hello-world' kind of dummy installer."
       homepage "https://github.com/robertfiko/homebrew-referl"
       url "https://github.com/robertfiko/referl/archive/0.5.tar.gz"
       sha256 "e5fae2d72d868d85662d4485334b60d0bf28f15eb845a99a22c883b2a2bd4426"
+      #version
       #license
     
-      bottle :unneeded
+      depends_on "yaws"
     
       def install
-        puts "Hello \n There \n I will \ Install"
-        system "g++", "main.cpp -o referl"
+        puts "I am now red".red
+        system "g++", "main.cpp", "-o", "referl"
         bin.install "referl"
       end
     
