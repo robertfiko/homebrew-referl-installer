93d6e0db313afdf8da7f883aacf7557011f7878a
Robert Fiko <fiko.robert@gmail.com>
2021-03-21 21:36:51 +0100

    cat

--- referl.rb
+++ referl.rb
@@ -11,7 +11,7 @@ class Referl < Formula
       #depends_on "gcc" => "4.9"
       #depends_on "yaws" => "1.89"
       #depends_on "graphviz" => "2.0"
-      depends_on ":grep"
+      depends_on :cat
     
       def install
         #find yaws path
