4b483c322baa4a420291441a2f499290b19b4484
Robert Fiko <fiko.robert@gmail.com>
2021-05-22 09:18:48 +0200

    Most rubocop problems fixed

--- referl.rb
+++ referl.rb
@@ -1,9 +1,9 @@
 # typed: false
 # frozen_string_literal: false
 
-# Bringing RefactorErl, a refactoring tool for Erlang to Homebrew
+# Refactoring software for Erlang language by ELTE and Ericson
 class Referl < Formula
-  desc "Refactoring brought to Erlang"
+  desc "Refactoring software for Erlang language by ELTE and Ericson"
   homepage "https://plc.inf.elte.hu/erlang/index.html"
   url "https://plc.inf.elte.hu/erlang/dl/refactorerl-0.9.20.08_v2.zip"
   version "0.9.20.08"
@@ -11,15 +11,19 @@ class Referl < Formula
   license "LGPL-3.0-only" # SPDX Representation for: GNU Lesser General Public License v3.0 only
 
   depends_on "erlang"
-  depends_on "gcc" => "4.9"
-  depends_on "graphviz" => "2.0"
-  depends_on "yaws" => "1.89"
+  depends_on "gcc" => "4.9.1"
+  depends_on "graphviz" => "2.47.1"
+  depends_on "yaws" => "2.0.9"
 
-  def install
-    # Variables
-    runner_script = "referl_exec"
+  # Creating exec script
+  def create_exec_script
+    out_file = File.new("bin/#{runner_script}", "w")
+    out_file.puts("#\!\/bin\/bash")
+    out_file.puts("#{String.new(HOMEBREW_PREFIX)}/Cellar/referl/+#{version}/bin/referl_boot -base #{String.new(HOMEBREW_PREFIX)}/Cellar/referl/#{version}/ $@")
+    out_file.close
+  end
 
-    # Yaws detect
+  def yaws_detect
     yaws_version = `yaws --version`.split[-1]
     yaws_path = "#{String.new(HOMEBREW_PREFIX)}/Cellar/yaws/#{yaws_version}/lib/yaws-#{yaws_version}/ebin"
     ohai "Looking for YAWS path on: #{yaws_path}"
@@ -28,36 +32,40 @@ class Referl < Formula
     else
       odie("Error! - yaws path not found, you may need to install one manually")
     end
+    yaws_path
+  end
 
-    # Creating exec script
-    out_file = File.new("bin/" + runner_script, "w")
-    out_file.puts("#\!\/bin\/bash")
-    out_file.puts("#{String.new(HOMEBREW_PREFIX)}/Cellar/referl/+#{version}/bin/referl_boot -base #{String.new(HOMEBREW_PREFIX)}/Cellar/referl/#{version}/ $@")
-    out_file.close
-
-    # Installing referl
-    system "bin/referl", "-build", "tool", "-yaws_path", yaws_path
-
-    # Getting install paths, and excluding the 'referl' start and 'referl_exec' exec script
+  def installpaths
     install_paths = []
     Dir["bin/*"].each { |x| install_paths.push(String.new(x)) }
     install_paths.delete("bin/referl")
-    install_paths.delete("bin/"+runner_script)
+    install_paths.delete("bin/#{runner_script}")
+    install_paths
+  end
 
-    # Installing binaries
+  def instal_referl
+    install_paths = installpaths
+
+    # Installing
     bin.install install_paths
     bin.install "bin/referl" => "referl_boot" # Rename referl to avoid conflicts with symlink
-    lib.install Dir["lib/*"] 
+    lib.install Dir["lib/*"]
     prefix.install "refactorerl.boot"
     prefix.install "sys.config"
 
-    bin.install "bin/"+runner_script => "referl" # Symlink will be pointed to this script, due to rename
-    
+    bin.install "bin/referl_exec" => "referl" # Symlink will be pointed to this script, due to rename
+  end
+
+  def install
+    yaws_path = yaws_detect
+    create_exec_script
+
+    system "bin/referl", "-build", "tool", "-yaws_path", yaws_path
+    instal_referl
   end
 
   test do
     assert_true(false)
-    # ########### Test will come later ############
-    # Főbb esetek: elindul, args, -yaws, -db, -name, doksiban több
+    # Fobb esetek: elindul, args, -yaws, -db, -name, doksiban tobb
   end
 end
