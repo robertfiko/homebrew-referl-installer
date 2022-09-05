afb6f304ef1ea8febc22d7af665ccc3f85a0b011
Robert Fiko <fiko.robert@gmail.com>
2022-09-05 15:49:19 +0200

    Change

--- referl@dev.rb
+++ referl@dev.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: false
 
 # Refactoring software for Erlang language by ELTE and Ericson
-class ReferlATdev < Formula
+class Referl@dev < Formula
   desc "Refactoring software for Erlang language by ELTE and Ericson"
   homepage "https://plc.inf.elte.hu/erlang/index.html"
   url "https://fikorobert.web.elte.hu/downloads/refels.zip"
